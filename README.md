56947e6948fc8cb725a64fff1ce29032d3d7d63d
Nate <fritz.nathaniel97@gmail.com>
2023-01-25 01:33:08 -0500

    creates package.json, test cases, and libraries to pass those tests

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Team-Profile-Gen
+# Team Profile Generator
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+## Project Description
+
+This application uses Jest for running the unit tests and Inquirer for collecting input from the user. It is a command-line application that takes in information about employees on a software engineering team and generates an HTML webpage that displays summaries for each person.
+
+## Installation
+
+Clone the repo and install all packages by running npm i in the root folder.
+
+## License
+
+This project is covered under the MIT License.
+
+## Contact
+
+Reach out to me via github!
+
+## Tests 
+
+After installing the jest package, use npm run test to run through the provided test files.
---
+++ _tests_/Employee.test.js
@@ -0,0 +1,24 @@
+const Employee = require('../lib/Employee')
+
+describe("Employee", () => {
+  describe("getAttributes", () => {
+    it("should create an Employee object and return the employee attributes passed through the input parameters", () => {
+
+      const testName = "Steve";
+      const testID = "999"
+      const testEmail = 'steve@aol.com'
+
+      const testEmployee = new Employee(testName, testID, testEmail);
+
+      nameResult = testEmployee.getName();
+      idResult = testEmployee.getId();
+      emailResult = testEmployee.getEmail()
+      roleResult = testEmployee.getRole()
+
+      expect(nameResult).toEqual(testName);
+      expect(idResult).toEqual(testID);
+      expect(emailResult).toEqual(testEmail);
+      expect(roleResult).toEqual('Employee');
+    });
+  });
+});
